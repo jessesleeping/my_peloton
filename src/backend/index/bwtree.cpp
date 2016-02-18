6bf237a2875db34b28b04a3a6c9cb90b375ac58b
Jiexi Lin <jxlin39@gmail.com>
2016-02-17 20:51:25 -0500

    add prototype

--- src/backend/index/bwtree.cpp
+++ src/backend/index/bwtree.cpp
@@ -16,6 +16,8 @@ namespace peloton {
 namespace index {
 
 // Add your function definitions here
+template <typename KeyType, typename ValueType, class KeyComparator>
+BWTree<KeyType, ValueType, KeyComparator>::Node BWTree<KeyType, ValueType, KeyComparator>::Node::lookup(KeyType k) { }
 
 }  // End index namespace
 }  // End peloton namespace
